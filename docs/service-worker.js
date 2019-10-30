bfdd23e4729a2da7268a50bf566d9cf1eb74cb6f
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-10-30 00:11:20 +0000

    Generate GitHub Pages

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/net-ecosystem-review/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
