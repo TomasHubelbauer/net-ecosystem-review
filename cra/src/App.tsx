a17fc1227a0e380a3fddce5b64d33ecb2a7ec6fe
Tomáš Hübelbauer <tomas@hubelbauer.net>
2020-02-20 12:21:51 +0100

    Add a missing space

--- cra/src/App.tsx
+++ cra/src/App.tsx
@@ -253,6 +253,7 @@ const App: FC = () => {
       <button onClick={handleNextButtonClick}>→</button>
       <input placeholder="🔍" value={filter} onChange={handleFilterInputChange} />
       Copyright &copy; <a href="https://hubelbauer.net">Tomas Hubelbauer</a> 2019.
+      {' '}
       <a href="https://github.com/TomasHubelbauer/net-ecosystem-review">GitHub</a>
       <hr />
       {renderSelectedRecord()}
