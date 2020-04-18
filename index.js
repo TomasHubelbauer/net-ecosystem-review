0599749ff7de88b9733929598178ddec221fe62c
Tomáš Hübelbauer <tomas@hubelbauer.net>
2020-04-18 10:05:40 +0200

    Catch async errors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fetch = require('node-fetch');
 const fs = require('fs-extra');
 const link = require('parse-link-header');
 
+process.on('unhandledRejection', error => { throw error; });
+
 void async function () {
   const stars = 1000;
   let links = { next: { url: `https://api.github.com/search/repositories?q=language:csharp+stars:>${stars}&sort=stars&order=desc` } };
@@ -10,6 +12,7 @@ void async function () {
     console.log('Fetching', links.next.url);
     const response = await fetch(links.next.url, { headers: { Authorization: 'token ' + process.argv[2] } });
     const data = await response.json();
+    console.log(data);
     links = link(response.headers.get('link'));
     for (const item of data.items) {
       text += `${item.id},${item.full_name},${item.stargazers_count},${item.html_url}\n`;
